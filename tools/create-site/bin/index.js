d9ab1243496b167d5cedc915c71de6f3e413e827
BGilRoces <bgilroces@uade.edu.ar>
2025-09-07 12:22:46 -0300

    chore: bootstrap monorepo KlikYListo

---
+++ tools/create-site/bin/index.js
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env node
+import fs from "fs-extra";
+import path from "path";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const argv = yargs(hideBin(process.argv)).parse();
+console.log("CLI create-site OK. Args:", argv);
