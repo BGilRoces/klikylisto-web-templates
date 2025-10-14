74c0fc97d47f33239370f7ef522ed0be2a2b8b6c
BGilRoces <bgilroces@uade.edu.ar>
2025-10-14 15:55:55 -0300

    rename

--- tools/create-site/bin/index.js
+++
@@ -1,8 +0,0 @@
-﻿#!/usr/bin/env node
-import fs from "fs-extra";
-import path from "path";
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
-
-const argv = yargs(hideBin(process.argv)).parse();
-console.log("CLI create-site OK. Args:", argv);
